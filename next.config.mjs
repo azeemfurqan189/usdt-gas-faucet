074addbf52afcbda52f21a2750b94bf75fe8a0a3
Azeem Furqan <azeemfurqan189@gmail.com>
2026-04-19 17:47:26 -0700

    Fixed next.config.mjs - removed invalid swcMinify option

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  swcMinify: false,
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    esmExternals: 'loose',     // Yeh warning fix karega
   },
 };
 
